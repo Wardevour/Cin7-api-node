f624848d1351504791b77974a8a479324b68b5fb
Kenneth Brockert <kbrockertii@gmail.com>
2019-03-16 03:29:42 -0400

    add more tests

--- test/products.spec.js
+++ test/products.spec.js
@@ -2,6 +2,8 @@
 
 const fs = require("fs");
 const path = require("path");
+const assert = require("chai").assert;
+const sleep = require("../lib/helpers").sleep;
 const Cin7 = require("../index");
 
 var cin7 = null;
@@ -25,6 +27,73 @@ describe("Cin7.products", function() {
                     //   or the file couldn't be created
                     fs.writeFileSync(fileName, response.body, "utf-8");
 
+                    done();
+                }).catch(done);
+            });
+
+            it("should create output/products.csv", function(done) {
+                this.timeout(5000);
+
+                cin7.products.get().then((response) => {
+                    // parse the json encoded string into an object
+                    var data = JSON.parse(response.body);
+
+                    var rows = [];
+                    rows[0] = Object.keys(data[0]).join(",");
+
+                    for (let i = 1; i < data.length; i++) {
+                        let values = Object.values(data[i]);
+
+                        // wrap values in single quotes
+                        values = values.map((val) => {
+                            return "'" + val + "'";
+                        });
+
+                        rows[i] = values.join(",");
+                    }
+
+                    // write the csv file
+                    var fileName = "output" + path.sep + "products.csv";
+                    fs.writeFileSync(fileName, rows.join("\n"), "utf-8");
+
+                    done();
+                }).catch(done);
+            });
+        });
+
+        context("when using page options", function() {
+            it("should get two pages and concat them", function(done) {
+                this.timeout(6000);
+
+                var rows = [];
+
+                var options = {
+                    "fields": ["id"],
+                    "rows": 1,
+                    "order": ["id ASC"],
+                    "page": 1
+                };
+
+                cin7.products.get(options).then((response) => {
+                    // parse the json encoded string into an object
+                    var data = JSON.parse(response.body);
+                    rows = rows.concat(data);
+
+                    options.page ++;
+                    assert.isAbove(options.page, 1);
+
+                    // Calls are limited to 1 per second, 60 per minute
+                    //   and 5000 per day. If you exceed this rate limit you
+                    //   will receive a HTTP 429 (Too Many Requests) response.
+                    sleep(1000);
+                    return cin7.products.get(options);
+                }).then((response) => {
+                    // parse the json encoded string into an object
+                    var data = JSON.parse(response.body);
+                    rows = rows.concat(data);
+
+                    assert.isAbove(rows.length, 1);
+                    assert.isAbove(rows[1].id, rows[0].id);
                     done();
                 }).catch(done);
             });
