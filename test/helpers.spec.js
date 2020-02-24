77b6f303e3ef98f2bb210e2d9acd0fd09b7f174e
Kenneth Brockert <kbrockertii@gmail.com>
2020-02-24 05:36:49 -0500

    Removed request and replaced it with node-fetch

--- test/helpers.spec.js
+++ test/helpers.spec.js
@@ -1,27 +1,27 @@
-"use strict";
+'use strict';
 
-const assert = require("chai").assert;
-const helpers = require("../lib/helpers");
+const assert = require('chai').assert;
+const helpers = require('../lib/helpers');
 
 
-describe("helpers", function() {
-    describe("#optionsToQueryString()", function() {
-        context("when generating a query string", function() {
-            it("should create qs object", function() {
+describe('helpers', function() {
+    describe('#optionsToQueryString()', function() {
+        context('when generating a query string', function() {
+            it('should create qs object', function() {
                 var testOptions = {
-                    "fields": ["Name", "Description"],
-                    "where": "Status = 1",
-                    "order": ["CreatedDate", "Name ASC"],
-                    "page": 1,
-                    "rows": 50
+                    'fields': ['Name', 'Description'],
+                    'where': 'Status = 1',
+                    'order': ['CreatedDate', 'Name ASC'],
+                    'page': 1,
+                    'rows': 50
                 };
 
                 var assertion = {
-                    "fields": "Name,Description",
-                    "where": "Status = 1",
-                    "order": "CreatedDate,Name ASC",
-                    "page": 1,
-                    "rows": 50
+                    'fields': 'Name,Description',
+                    'where': 'Status = 1',
+                    'order': 'CreatedDate,Name ASC',
+                    'page': 1,
+                    'rows': 50
                 };
 
                 var testData = helpers.optionsToQueryString(testOptions);
